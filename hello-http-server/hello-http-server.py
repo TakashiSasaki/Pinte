030c911be5fc1677334e4e50292c676e431cb01b
Takashi SASAKI <takashi316@gmail.com>
2015-03-10 21:14:32 +0900

    indentation is changed to multiple of four

--- hello-http-server/hello-http-server.py
+++ hello-http-server/hello-http-server.py
@@ -4,20 +4,18 @@ import http.server, http.cookies
 SERVER_ADDRESS = ""
 SERVER_PORT = 13579
 
+
 def main():
-  server_address = (SERVER_ADDRESS, SERVER_PORT)
-   
-  http_server = http.server.HTTPServer((SERVER_ADDRESS, SERVER_PORT), MyRequestHandler)
-  http_server.handle_request()
+    http_server = http.server.HTTPServer((SERVER_ADDRESS, SERVER_PORT), MyRequestHandler)
+    http_server.handle_request()
    
 class MyRequestHandler(http.server.BaseHTTPRequestHandler):
-  def do_GET(self): 
-    self.wfile.write(bytes("Hello.\r\n", "UTF-8"))
-    if "cookie" in self.headers:
-      self.cookie=Cookie.SimpleCookie(self.headers.getheader("cookie"))
-      for x in self.cookie:
-        self.wfile.write(bytes(x + "\r\n", "UTF-8"));
+    def do_GET(self):
+        self.wfile.write(bytes("Hello.\r\n", "UTF-8"))
+        if "cookie" in self.headers:
+            self.cookie=http.cookies.SimpleCookie(self.headers.getheader("cookie"))
+            for x in self.cookie:
+                self.wfile.write(bytes(x + "\r\n", "UTF-8"))
 
 if __name__ == "__main__":
-  main()
-
+    main()
