64de04dec940645faedd51fefd3d91af590b82f2
Takashi SASAKI <takashi316@gmail.com>
2015-03-10 21:25:54 +0900

    print current time every time

--- hello-http-server/hello-http-server.py
+++ hello-http-server/hello-http-server.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 import http.server, http.cookies
+import time
 
 SERVER_ADDRESS = ""
 SERVER_PORT = 13579
 
 
-
 class MyRequestHandler(http.server.BaseHTTPRequestHandler):
     def do_GET(self):
-        self.wfile.write(bytes("Hello.\r\n", "UTF-8"))
+        self.wfile.write(bytes("Hello at " + time.asctime() + "\r\n", "UTF-8"))
         if "cookie" in self.headers:
             self.cookie=http.cookies.SimpleCookie(self.headers.getheader("cookie"))
             for x in self.cookie:
@@ -18,4 +18,4 @@ if __name__ == "__main__":
     http_server = http.server.HTTPServer((SERVER_ADDRESS, SERVER_PORT), MyRequestHandler)
     while True:
         http_server.handle_request()
-        
+        print(time.asctime())
