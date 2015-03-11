24a23bd4c1c70392832a16d72d9ea7c472f24a0b
Takashi SASAKI <takashi316@gmail.com>
2015-03-11 23:26:10 +0900

    load and save cookie into and from file

--- pint-web.py
+++ pint-web.py
@@ -4,6 +4,7 @@ import http.cookies
 import http.cookiejar
 import time
 import sys
+import logging
 
 
 SERVER_ADDRESS = ""
@@ -20,6 +21,9 @@ class MyRequestHandler(http.server.BaseHTTPRequestHandler):
         print("simple_cookie_in_request")
         print(simple_cookie_in_request)
 
+        saveSimpleCookie(simple_cookie_in_request)
+        loadSimpleCookie()
+
         self.send_response(200)
         self.send_header("Content-type", "text/html")
         simple_cookie_in_response = http.cookies.SimpleCookie()
@@ -33,6 +37,18 @@ class MyRequestHandler(http.server.BaseHTTPRequestHandler):
         self.end_headers()
 
         self.wfile.write(bytes("Hello at " + time.asctime() + "\r\n", "UTF-8"))
+
+def saveSimpleCookie(simple_cookie):
+    f = open("simple_cookie.txt", "w")
+    f.write(simple_cookie.output())
+    f.close()
+
+def loadSimpleCookie():
+    f = open("simple_cookie.txt", "r")
+    simple_cookie = http.cookies.SimpleCookie()
+    simple_cookie.load(f.read())
+    print("loadSimpleCookie")
+    print(simple_cookie)
 
 if __name__ == "__main__":
     http_server = http.server.HTTPServer((SERVER_ADDRESS, SERVER_PORT), MyRequestHandler)
