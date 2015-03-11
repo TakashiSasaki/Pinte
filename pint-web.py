4d53a9ac6a955dd3e97dc52996dfbfcb9745119c
Takashi SASAKI <takashi316@gmail.com>
2015-03-11 22:58:53 +0900

    cookie handling in request and response

--- pint-web.py
+++ pint-web.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
-import http.server, http.cookies
-import time, sys
+import http.server
+import http.cookies
+import http.cookiejar
+import time
+import sys
+
 
 SERVER_ADDRESS = ""
 SERVER_PORT = 13579
@@ -8,11 +12,27 @@ SERVER_PORT = 13579
 
 class MyRequestHandler(http.server.BaseHTTPRequestHandler):
     def do_GET(self):
+        print("do_GET\n")
+        print(self.headers)
+
+        simple_cookie_in_request = http.cookies.SimpleCookie()
+        simple_cookie_in_request.load(self.headers["Cookie"])
+        print("simple_cookie_in_request")
+        print(simple_cookie_in_request)
+
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        simple_cookie_in_response = http.cookies.SimpleCookie()
+        simple_cookie_in_response["responded_time"] = time.asctime()
+        simple_cookie_in_response["hello"] = "world"
+
+        print("simple_cookie_in_response")
+        print(simple_cookie_in_response)
+        for x in simple_cookie_in_response:
+            self.send_header("Set-Cookie", simple_cookie_in_response[x].OutputString())
+        self.end_headers()
+
         self.wfile.write(bytes("Hello at " + time.asctime() + "\r\n", "UTF-8"))
-        if "cookie" in self.headers:
-            self.cookie=http.cookies.SimpleCookie(self.headers.getheader("cookie"))
-            for x in self.cookie:
-                self.wfile.write(bytes(x + "\r\n", "UTF-8"))
 
 if __name__ == "__main__":
     http_server = http.server.HTTPServer((SERVER_ADDRESS, SERVER_PORT), MyRequestHandler)
