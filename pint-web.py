176d859db5ad66f81b35161611ba67d3706a179d
Takashi SASAKI <takashi316@gmail.com>
2015-03-11 23:39:43 +0900

    logger replaces print function

--- pint-web.py
+++ pint-web.py
@@ -5,7 +5,7 @@ import http.cookiejar
 import time
 import sys
 import logging
-
+logging.basicConfig(level=logging.INFO)
 
 SERVER_ADDRESS = ""
 SERVER_PORT = 13579
@@ -13,13 +13,13 @@ SERVER_PORT = 13579
 
 class MyRequestHandler(http.server.BaseHTTPRequestHandler):
     def do_GET(self):
-        print("do_GET\n")
-        print(self.headers)
+        logging.getLogger().info("do_GET\n")
+        logging.getLogger().info(self.headers)
 
         simple_cookie_in_request = http.cookies.SimpleCookie()
         simple_cookie_in_request.load(self.headers["Cookie"])
-        print("simple_cookie_in_request")
-        print(simple_cookie_in_request)
+        logging.getLogger().info("simple_cookie_in_request")
+        logging.getLogger().info(simple_cookie_in_request)
 
         saveSimpleCookie(simple_cookie_in_request)
         loadSimpleCookie()
@@ -30,8 +30,8 @@ class MyRequestHandler(http.server.BaseHTTPRequestHandler):
         simple_cookie_in_response["responded_time"] = time.asctime()
         simple_cookie_in_response["hello"] = "world"
 
-        print("simple_cookie_in_response")
-        print(simple_cookie_in_response)
+        logging.getLogger().info("simple_cookie_in_response")
+        logging.getLogger().info(simple_cookie_in_response)
         for x in simple_cookie_in_response:
             self.send_header("Set-Cookie", simple_cookie_in_response[x].OutputString())
         self.end_headers()
@@ -47,11 +47,11 @@ def loadSimpleCookie():
     f = open("simple_cookie.txt", "r")
     simple_cookie = http.cookies.SimpleCookie()
     simple_cookie.load(f.read())
-    print("loadSimpleCookie")
-    print(simple_cookie)
+    logging.getLogger().info("loadSimpleCookie")
+    logging.getLogger().info(simple_cookie)
 
 if __name__ == "__main__":
     http_server = http.server.HTTPServer((SERVER_ADDRESS, SERVER_PORT), MyRequestHandler)
     while True:
-        print("handle_request at " + time.asctime(), file=sys.stderr)
+        logging.getLogger().info("handle_request at " + time.asctime())
         http_server.handle_request()
