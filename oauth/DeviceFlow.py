8822a8551a36fe2201eb7aa098880b13bd675e26
Takashi SASAKI <takashi316@gmail.com>
2015-03-19 19:30:06 +0900

    DeviceFlow works

--- oauth/DeviceFlow.py
+++ oauth/DeviceFlow.py
@@ -7,6 +7,7 @@ import urllib.parse
 import http.client
 import webbrowser
 from logging import getLogger
+import time
 
 
 class DeviceFlow(object):
@@ -49,16 +50,32 @@ class DeviceFlow(object):
         self.logger.debug("opened web browser")
 
     def getAccessToken(self):
+        while True:
+            self.logger.debug("sleeping %s sec" % self.interval)
+            time.sleep(self.interval)
+            try:
+                self.getAccessTokenOnce()
+            except urllib.error.HTTPError as e:
+                self.logger.debug(e)
+                continue
+
+    def getAccessTokenOnce(self):
+        self.logger.debug("getAccessTokenOnce")
         data = {}
         data["client_id"] = self.client_id
         data["client_secret"] = self.client_secret
         data["code"] = self.device_code
         data["grant_type"] = "http://oauth.net/grant_type/device/1.0"
+        self.logger.debug(data)
         response = urllib.request.urlopen("https://www.googleapis.com/oauth2/v3/token",
                                           bytes(urllib.parse.urlencode(data), "UTF-8"))
         assert isinstance(response, http.client.HTTPResponse)
         j = json.loads(response.readall().decode("UTF-8"))
-        self.logger("getAccessToken: %s" % str(j))
+        self.logger.debug("getAccessToken: %s" % str(j))
+        self.access_token = j["access_token"]
+        self.expires_in = j["expires_in"]
+        self.refresh_token = j["refresh_token"]
+        self.token_type = j["token_type"]
 
     def getAccessTokenRepeatedly(self):
         pass
@@ -79,3 +96,4 @@ if __name__ == "__main__":
     device_flow.testLog()
     device_flow.getUserCode(SCOPE)
     device_flow.verifyUserCode()
+    device_flow.getAccessToken()
