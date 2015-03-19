fcfb83fefbe89415de642ab65db8bf778cd247b8
Takashi SASAKI <takashi316@gmail.com>
2015-03-19 18:24:14 +0900

    class HelloLogging works

--- misc/HelloLogging.py
+++ misc/HelloLogging.py
@@ -1,25 +1,40 @@
 import logging
 
-logging.debug("root-debug")
-logging.info("root-info")
-logging.warning("root-warn")
-logging.error("root-error")
-logging.critical("root-critical")
+def logByModuleLevelLogger():
+    logging.debug("root-debug")
+    logging.info("root-info")
+    logging.warning("root-warn")
+    logging.error("root-error")
+    logging.critical("root-critical")
 
-a = logging.getLogger()
-a.debug("default-debug")
-a.info("default-info")
-a.warning("default-warn")
-a.error("default-error")
-a.critical("default-critical")
-a.setLevel(logging.DEBUG)
-a.debug("default-debug")
+def logByDefaultLogger():
+    a = logging.getLogger()
+    a.debug("default-debug")
+    a.info("default-info")
+    a.warning("default-warn")
+    a.error("default-error")
+    a.critical("default-critical")
+    a.setLevel(logging.DEBUG)
+    a.debug("default-debug")
 
-b = logging.getLogger("b")
-b.debug("b-debug")
-b.info("b-info")
-b.warning("b-warn")
-b.error("b-error")
-b.critical("b-critical")
-b.setLevel(logging.DEBUG)
-b.debug("b-debug")
+def logByNamedLogger():
+    b = logging.getLogger("b")
+    b.debug("b-debug")
+    b.info("b-info")
+    b.warning("b-warn")
+    b.error("b-error")
+    b.critical("b-critical")
+    b.setLevel(logging.DEBUG)
+    b.debug("b-debug")
+
+class ClassA(object):
+    def __init__(self):
+        self.logger = logging.getLogger("ClassA")
+        self.logger.warning("ClassA.__init__")
+
+    def methodA(self):
+        self.logger.warning("ClassA.methodA")
+
+if __name__ == "__main__":
+    class_a = ClassA()
+    class_a.methodA()
